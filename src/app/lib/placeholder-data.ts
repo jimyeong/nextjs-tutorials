84b224b9679b2da439a066553829362ffe14c8b0
JIMMY JUNG <idjjm92@gmail.com>
2024-09-27 14:15:47 +0100

    first commit

---
+++ src/app/lib/placeholder-data.ts
@@ -0,0 +1,24 @@
+const customers = [
+  {
+    id: 0,
+  },
+  {
+    id: 1,
+  },
+];
+
+const invoices = [
+  {
+    customer_id: customers[0].id,
+    amount: 15795,
+    status: "pending",
+    date: "2022-12-06",
+  },
+  {
+    customer_id: customers[1].id,
+    amount: 20348,
+    status: "pending",
+    date: "2022-11-14",
+  },
+  // ...
+];
